1bc5e9dfe3cfce82a601e05c0bb12609157b74d0
Ayoub Taouaf <ataouaf@e1r1p5.1337.ma>
2023-10-11 18:46:12 +0100

    fix something in repo

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abizyane <abizyane@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 06:06:43 by ataouaf           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/09/21 10:18:11 by ataouaf          ###   ########.fr       */
-=======
-/*   Updated: 2023/10/05 12:05:01 by abizyane         ###   ########.fr       */
->>>>>>> ca246a9c0555e22559a5a417e874b1675d44c3f7
+/*   Updated: 2023/10/11 18:43:49 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +22,8 @@
 # include <string.h>
 # include <unistd.h>
 
-<<<<<<< HEAD
-# define WIDTH 1920
-# define HEIGHT 1080
-=======
 # define WIDTH 1280
 # define HEIGHT 720
->>>>>>> ca246a9c0555e22559a5a417e874b1675d44c3f7
 # define TILE_SIZE 64
 # define FOV_ANGLE 60
 # define WALL_STRIP_WIDTH 1
@@ -45,15 +36,6 @@
 #  define BUFFER_SIZE 42
 # endif
 
-<<<<<<< HEAD
-typedef struct s_float_vect
-{
-	float			x;
-	float			y;
-}					t_float_vect;
-
-typedef struct s_int_vect
-=======
 typedef struct s_vect
 {
 	double			x;
@@ -61,7 +43,6 @@ typedef struct s_vect
 }					t_vect;
 
 typedef struct s_int_vect //TODO: hadi kon ghi 7ayadnaha 
->>>>>>> ca246a9c0555e22559a5a417e874b1675d44c3f7
 {
 	int				x;
 	int				y;
@@ -106,29 +87,17 @@ typedef enum e_direction
 typedef struct s_raycast
 {
 	double		ray_angle;
-<<<<<<< HEAD
-	t_float_vect	horizontal_step;
-	t_float_vect	horizontal_wall_hit;
-	t_float_vect	vertical_step;
-	t_float_vect	vertical_wall_hit;
-	float		distance;
-=======
 	t_vect	horizontal_step;
 	t_vect	horizontal_wall_hit;
 	t_vect	vertical_step;
 	t_vect	vertical_wall_hit;
 	double		distance;
->>>>>>> ca246a9c0555e22559a5a417e874b1675d44c3f7
 	int		hit_vertical;
 	int		hit_horizontal;
 	int		ray_facing_up;
 	int		ray_facing_right;
 	t_direction	wall_direction;
-<<<<<<< HEAD
-	float		wall_height;
-=======
 	double		wall_height;
->>>>>>> ca246a9c0555e22559a5a417e874b1675d44c3f7
 	int			draw_start;
 	int			draw_end;
 }					t_raycast;
@@ -143,8 +112,6 @@ typedef struct s_parse
 	int				floor;
 	int				ceil;
 }					t_parse;
-<<<<<<< HEAD
-=======
 
 typedef struct s_player
 {
@@ -157,20 +124,9 @@ typedef struct s_player
 	double			rotation_speed;
 	double			fov;
 }					t_player;
->>>>>>> ca246a9c0555e22559a5a417e874b1675d44c3f7
 
 typedef struct s_player
 {
-<<<<<<< HEAD
-	t_float_vect	pos;
-	float			turn_dir;
-	int				walk_dir;
-	int				strafe_dir;
-	float			rotation_angle;
-	float			move_speed;
-	float			rotation_speed;
-}					t_player;
-=======
 	t_mlx			mlx;
 	t_parse			*parse;
 	t_raycast		*rays;
@@ -215,7 +171,6 @@ void 			ft_draw_walls(t_cube *cube, t_raycast *rays);
 // uint32_t			ft_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t a);
 void 			ft_player_movement(void *param);
 void				ft_minimap(void *param);
->>>>>>> ca246a9c0555e22559a5a417e874b1675d44c3f7
 
 typedef struct s_cube
 {
